e99ef5a2d313e7c5fddb73c90903fb5486627f7d
mifmasterz <mifmasterz@yahoo.com>
2022-06-29 13:09:38 +0700

    init

---
+++ src/AquaCulture.Device/Program.cs
@@ -0,0 +1,167 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Modbus;
+using GHIElectronics.TinyCLR.Devices.Modbus.Interface;
+using GHIElectronics.TinyCLR.Devices.Network;
+using GHIElectronics.TinyCLR.Pins;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace AquaCulture.Device
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StartEthernet();
+            ModbusDevice ModbusTCP_Device;
+            ModbusTCP_Device = new ModbusClient(248);
+            ModbusTcpListener mbListner;
+            mbListner = new ModbusTcpListener(ModbusTCP_Device, 502, 5, 1000);
+            Thread.Sleep(100);
+            ModbusTCP_Device.Start();
+            Thread.Sleep(Timeout.Infinite);
+        }
+        static bool linkReady = false;
+
+        static void StartEthernet()
+        {
+            //Reset external phy.
+            var gpioController = GpioController.GetDefault();
+            var resetPin = gpioController.OpenPin(SC20260.GpioPin.PG3);
+
+            resetPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            resetPin.Write(GpioPinValue.Low);
+            Thread.Sleep(100);
+
+            resetPin.Write(GpioPinValue.High);
+            Thread.Sleep(100);
+
+            var networkController = NetworkController.FromName(SC20260.NetworkController.EthernetEmac);
+
+            var networkInterfaceSetting = new EthernetNetworkInterfaceSettings();
+
+            var networkCommunicationInterfaceSettings = new BuiltInNetworkCommunicationInterfaceSettings();
+
+            networkInterfaceSetting.Address = new IPAddress(new byte[] { 192, 168, 1, 122 });
+            networkInterfaceSetting.SubnetMask = new IPAddress(new byte[] { 255, 255, 255, 0 });
+            networkInterfaceSetting.GatewayAddress = new IPAddress(new byte[] { 192, 168, 1, 1 });
+
+            networkInterfaceSetting.DnsAddresses = new IPAddress[]
+                {new IPAddress(new byte[] { 75, 75, 75, 75 }),
+         new IPAddress(new byte[] { 75, 75, 75, 76 })};
+
+            networkInterfaceSetting.MacAddress = new byte[] { 0x00, 0x4, 0x00, 0x00, 0x00, 0x00 };
+            networkInterfaceSetting.DhcpEnable = true;
+            networkInterfaceSetting.DynamicDnsEnable = true;
+
+            networkController.SetInterfaceSettings(networkInterfaceSetting);
+            networkController.SetCommunicationInterfaceSettings(networkCommunicationInterfaceSettings);
+
+            networkController.SetAsDefaultController();
+
+            networkController.NetworkAddressChanged += NetworkController_NetworkAddressChanged;
+            networkController.NetworkLinkConnectedChanged += NetworkController_NetworkLinkConnectedChanged;
+
+            networkController.Enable();
+
+            while (linkReady == false) ;
+            Debug.WriteLine("Network is ready to use");
+            //Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void NetworkController_NetworkLinkConnectedChanged
+            (NetworkController sender, NetworkLinkConnectedChangedEventArgs e)
+        {
+            //Raise connect/disconnect event.
+        }
+
+        private static void NetworkController_NetworkAddressChanged
+            (NetworkController sender, NetworkAddressChangedEventArgs e)
+        {
+
+            var ipProperties = sender.GetIPProperties();
+            var address = ipProperties.Address.GetAddressBytes();
+
+            linkReady = address[0] != 0;
+            Debug.WriteLine("IP: " + address[0] + "." + address[1] + "." + address[2] + "." + address[3]);
+        }
+
+
+    }
+    public class ModbusClient : ModbusDevice
+    {
+        public ModbusClient(byte deviceAddress, object syncObject = null)
+           : base(deviceAddress, syncObject) { }
+
+        protected override string OnGetDeviceIdentification(ModbusObjectId objectId)
+        {
+            switch (objectId)
+            {
+                case ModbusObjectId.VendorName:
+                    return "GHI Electronics";
+                case ModbusObjectId.ProductCode:
+                    return "101";
+                case ModbusObjectId.MajorMinorRevision:
+                    return "1.0";
+                case ModbusObjectId.VendorUrl:
+                    return "ghielectronics.com";
+                case ModbusObjectId.ProductName:
+                    return "SitCore";
+                case ModbusObjectId.ModelName:
+                    return "SCM20260D";
+                case ModbusObjectId.UserApplicationName:
+                    return "Modbus Slave Test";
+            }
+            return null;
+        }
+
+        protected override ModbusConformityLevel GetConformityLevel()
+        {
+            return ModbusConformityLevel.Regular;
+        }
+
+        protected override ModbusErrorCode OnReadCoils(bool isBroadcast, ushort startAddress, ushort coilCount, byte[] coils)
+        {
+            try
+            {
+                
+                for (int n = 0; n < coils.Length; ++n)
+                {
+                    coils[n] = 1;
+                }
+                Debug.WriteLine("Master read coils");
+                return ModbusErrorCode.NoError;
+            }
+            catch
+            {
+                Debug.WriteLine("error in on read coils registers");
+                return base.OnReadCoils(isBroadcast, startAddress, coilCount, coils);
+            }
+        }
+
+        protected override ModbusErrorCode OnReadHoldingRegisters(bool isBroadcast, ushort startAddress, ushort[] registers)
+        {
+            try
+            {
+                ushort counter = 1;
+                for (int n = 0; n < registers.Length; ++n)
+                {
+                    registers[n] = counter++; // set number in each register for testing               
+                }
+                Debug.WriteLine("Master Read Holding Registers - " + registers[0].ToString());
+                return ModbusErrorCode.NoError;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("error in on read holding registers");
+                return base.OnReadHoldingRegisters(isBroadcast, startAddress, registers);
+            }
+        }
+
+    }
+}
---
+++ src/AquaCulture.Gateway/Program.cs
@@ -0,0 +1,49 @@
+﻿using AquaCulture.Gateway;
+using System.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int DelayTime = 1000;
+        var mqtt = new MqttService();
+        mqtt.MessageReceived += (e, o) => {
+            Console.WriteLine(o.Topic +" => "+ o.Message);
+        };
+        mqtt.IsSubscribed = true;
+        int.TryParse(ConfigurationManager.AppSettings["Delay"],out DelayTime);
+        var DeviceID = ConfigurationManager.AppSettings["DeviceID"];
+        var IPModbusDevice = ConfigurationManager.AppSettings["IPModbusDevice"];
+        EasyModbus.ModbusClient modbusClient = new EasyModbus.ModbusClient(IPModbusDevice, 502);
+        //Increase the Connection Timeout to 5 seconds
+        modbusClient.ConnectionTimeout = 5000;
+        //We create a Log File. This will also active the Event logging 
+        modbusClient.LogFileFilename = "test.txt";
+        //The Messages sent to the MQTT-Broker will be retained in the Broker. After subscribtion, the client will automatically
+        //receive the last retained message. By default the Retain-Flag is FALSE -> Messages will not be retained
+        //modbusClient.MqttRetainMessages = true;
+        //Connect to the ModbusTCP Server
+        modbusClient.Connect();
+        while (true)
+        {
+            // We read two registers from the Server, and Publish them to the MQTT-Broker. By default Values will be published
+            // on change By default we publish to the Topic 'easymodbusclient/' and the the address e.g. ''easymodbusclient/holdingregister1'
+            // The propery "Password" and "Username" can be used to connect to a Broker which require User and Password. By default the 
+            //MQTT-Broker port is 1883
+            Console.WriteLine("read holding registers");
+            int[] holdingRegister = modbusClient.ReadHoldingRegisters(60, 10);
+            foreach(var num in holdingRegister)
+                Console.WriteLine($"read = {num}");
+            Console.WriteLine("read coils");
+            bool[] coils = modbusClient.ReadCoils(10, 10);
+            foreach(var num in coils)
+                Console.WriteLine($"read = {num}");
+            mqtt.SendTelemetry(DeviceID, coils, holdingRegister);
+            System.Threading.Thread.Sleep(DelayTime);
+        }
+        modbusClient.Disconnect();
+        Console.ReadKey();
+        
+
+    }
+}
