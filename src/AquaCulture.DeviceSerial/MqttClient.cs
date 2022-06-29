a7648e36f4357a42e90b9db585af2da26f29bbea
mifmasterz <mifmasterz@yahoo.com>
2022-06-29 14:14:15 +0700

    add rtu device

---
+++ src/AquaCulture.DeviceSerial/MqttClient.cs
@@ -0,0 +1,110 @@
+﻿using GHIElectronics.TinyCLR.Data.Json;
+using GHIElectronics.TinyCLR.Networking.Mqtt;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace AquaCulture.DeviceSerial
+{
+    public class MqttMessageReceivedEventArgs : EventArgs
+    {
+        public string Topic { get; set; }
+        public string Message { get; set; }
+    }
+    public class MqttClient
+    {
+        public delegate void MessageReceivedHandler(object sender, MqttMessageReceivedEventArgs e);
+        public event MessageReceivedHandler MessageReceived;
+
+        string topic = "aqua/data";
+        Mqtt client;
+        public MqttClient()
+        {
+            
+
+        }
+
+        public void Connect()
+        {
+            var caCertificate = new X509Certificate(UTF8Encoding.UTF8.GetBytes("Your certificate"));
+
+            var mqttHost = "broker.emqx.io";
+            var mqttPort = 8883; //default 8883
+            var deviceId = "sensor-device-001";
+
+            var username = "device-001";
+            var password = "mqtt_pass";
+           
+
+            try
+            {
+                var clientSetting = new MqttClientSetting
+                {
+                    BrokerName = mqttHost,
+                    BrokerPort = mqttPort,
+                    ClientCertificate = null,
+                    CaCertificate = caCertificate,
+                    SslProtocol = System.Security.Authentication.SslProtocols.Tls12
+                };
+
+                client = new Mqtt(clientSetting);
+
+                var connectSetting = new MqttConnectionSetting
+                {
+                    ClientId = deviceId,
+                    UserName = username,
+                    Password = password
+                };
+
+                // Connect to host
+                var returnCode = client.Connect(connectSetting);
+
+                var packetId = 1;
+
+                // Subscribe to a topic
+                client.Subscribe(new string[] { topic }, new QoSLevel[] { QoSLevel.ExactlyOnce },
+                    (ushort)packetId++);
+
+               
+
+                // Publish recieved change from a specific topic
+                client.PublishReceivedChanged += (object sender, string topic, byte[] data, bool duplicate, QoSLevel qosLevel, bool retain) => {
+                    var message = Encoding.UTF8.GetString(data);
+                    if (MessageReceived != null)
+                    {
+                        MessageReceived.Invoke(this, new MqttMessageReceivedEventArgs() { Message = message, Topic = topic });
+                    }
+                };
+
+                client.PublishedChanged += (a, b, c) => { Debug.WriteLine("Published Changed."); };
+                client.SubscribedChanged += (a, b, c) => { Debug.WriteLine("Subscribed Changed."); };
+                client.ConnectedChanged += (a) => { Debug.WriteLine("Connected Changed."); };
+                client.UnsubscribedChanged += (a, b) => { Debug.WriteLine("Unsubscribed Changed."); };
+
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+        }
+        public void SendTelemetry(string DeviceId, byte[] Coils, ushort[] Registers)
+        {
+            var action = new DeviceTwin() { DeviceID = DeviceId, Coils = Coils, Registers = Registers };
+            var message = JsonConverter.Serialize(action);
+            var packetId = 1;
+            client.Publish(topic, Encoding.UTF8.GetBytes(message.ToString()), QoSLevel.MostOnce, false, (ushort)packetId++);
+        }
+       
+    }
+
+    public class DeviceTwin
+    {
+        public string DeviceID { get; set; }
+        public byte[] Coils { get; set; }
+        public ushort[] Registers { get; set; }
+    }
+}
--- src/AquaCulture.DeviceSerial/Program.cs
+++ src/AquaCulture.DeviceSerial/Program.cs
@@ -2,6 +2,7 @@
 using GHIElectronics.TinyCLR.Devices.Modbus;
 using GHIElectronics.TinyCLR.Devices.Modbus.Interface;
 using GHIElectronics.TinyCLR.Devices.Network;
+using GHIElectronics.TinyCLR.Devices.Uart;
 using GHIElectronics.TinyCLR.Pins;
 using System;
 using System.Collections;
@@ -10,20 +11,85 @@ using System.Net;
 using System.Text;
 using System.Threading;
 
-namespace AquaCulture.Device
+namespace AquaCulture.DeviceSerial
 {
     internal class Program
     {
+        static int DelayTime = 1000;
+        static string DeviceId = "device-001";
+        static MqttClient client;
         static void Main()
         {
             StartEthernet();
-            ModbusDevice ModbusTCP_Device;
-            ModbusTCP_Device = new ModbusClient(248);
-            ModbusTcpListener mbListner;
-            mbListner = new ModbusTcpListener(ModbusTCP_Device, 502, 5, 1000);
-            Thread.Sleep(100);
-            ModbusTCP_Device.Start();
+            client = new MqttClient();
+            client.Connect();
+            Thread th1 = new Thread(new ThreadStart(StartReading));
+            th1.Start();
             Thread.Sleep(Timeout.Infinite);
+
+        }
+        static void StartReading()
+        {
+
+            var serial = UartController.FromName(SC20260.UartPort.Uart5);
+
+            var uartSetting = new UartSetting()
+            {
+                BaudRate = 19200,
+                DataBits = 8,
+                Parity = UartParity.None,
+                StopBits = UartStopBitCount.One,
+                Handshaking = UartHandshake.None,
+            };
+
+            serial.SetActiveSettings(uartSetting);
+
+            serial.Enable();
+
+            IModbusInterface mbInterface;
+            mbInterface = new ModbusRtuInterface(
+                serial,
+                19200,
+                8,
+                UartStopBitCount.One,
+                UartParity.None);
+
+            ModbusMaster mbMaster;
+            mbMaster = new ModbusMaster(mbInterface);
+
+            var mbTimeout = false;
+
+            ushort[] registers = null;
+            //int count = 0;
+            byte[] coils;
+            while (true)
+            {
+                try
+                {
+                    mbTimeout = false;
+
+                    registers = mbMaster.ReadHoldingRegisters(10, 0, 1, 3333);
+                    coils = mbMaster.ReadCoils(10, 0, 2, 3333);
+
+                    client.SendTelemetry(DeviceId, coils, registers);
+                    //count++;
+
+                    //if (count == 5)
+                    //    break;
+                }
+                catch (System.Exception error)
+                {
+                    Debug.WriteLine("Modbus Timeout");
+                    mbTimeout = true;
+                }
+
+                if (!mbTimeout)
+                {
+                    Debug.WriteLine("Modbus : " + (object)registers[0].ToString());
+                }
+
+                Thread.Sleep(DelayTime);
+            }
         }
         #region network
         static bool linkReady = false;
@@ -54,7 +120,7 @@ namespace AquaCulture.Device
 
             networkInterfaceSetting.DnsAddresses = new IPAddress[]
                 {new IPAddress(new byte[] { 75, 75, 75, 75 }),
-         new IPAddress(new byte[] { 75, 75, 75, 76 })};
+         new IPAddress(new byte[] { 8, 8, 8, 8 })};
 
             networkInterfaceSetting.MacAddress = new byte[] { 0x00, 0x4, 0x00, 0x00, 0x00, 0x00 };
             networkInterfaceSetting.DhcpEnable = true;
@@ -92,77 +158,7 @@ namespace AquaCulture.Device
             Debug.WriteLine("IP: " + address[0] + "." + address[1] + "." + address[2] + "." + address[3]);
         }
         #endregion
-
     }
-    public class ModbusClient : ModbusDevice
-    {
-        public ModbusClient(byte deviceAddress, object syncObject = null)
-           : base(deviceAddress, syncObject) { }
 
-        protected override string OnGetDeviceIdentification(ModbusObjectId objectId)
-        {
-            switch (objectId)
-            {
-                case ModbusObjectId.VendorName:
-                    return "GHI Electronics";
-                case ModbusObjectId.ProductCode:
-                    return "101";
-                case ModbusObjectId.MajorMinorRevision:
-                    return "1.0";
-                case ModbusObjectId.VendorUrl:
-                    return "ghielectronics.com";
-                case ModbusObjectId.ProductName:
-                    return "SitCore";
-                case ModbusObjectId.ModelName:
-                    return "SCM20260D";
-                case ModbusObjectId.UserApplicationName:
-                    return "Modbus Slave Test";
-            }
-            return null;
-        }
 
-        protected override ModbusConformityLevel GetConformityLevel()
-        {
-            return ModbusConformityLevel.Regular;
-        }
-
-        protected override ModbusErrorCode OnReadCoils(bool isBroadcast, ushort startAddress, ushort coilCount, byte[] coils)
-        {
-            try
-            {
-
-                for (int n = 0; n < coils.Length; ++n)
-                {
-                    coils[n] = 1;
-                }
-                Debug.WriteLine("Master read coils");
-                return ModbusErrorCode.NoError;
-            }
-            catch
-            {
-                Debug.WriteLine("error in on read coils registers");
-                return base.OnReadCoils(isBroadcast, startAddress, coilCount, coils);
-            }
-        }
-
-        protected override ModbusErrorCode OnReadHoldingRegisters(bool isBroadcast, ushort startAddress, ushort[] registers)
-        {
-            try
-            {
-                ushort counter = 1;
-                for (int n = 0; n < registers.Length; ++n)
-                {
-                    registers[n] = counter++; // set number in each register for testing               
-                }
-                Debug.WriteLine("Master Read Holding Registers - " + registers[0].ToString());
-                return ModbusErrorCode.NoError;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("error in on read holding registers");
-                return base.OnReadHoldingRegisters(isBroadcast, startAddress, registers);
-            }
-        }
-
-    }
 }
